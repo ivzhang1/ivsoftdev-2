b63802ff428d04677bf7817c5dc08860f2a01a29
ivzhang1 <izhang1@stuy.edu>
2019-01-30 21:53:14 -0500

    work

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,21 +1,34 @@
-//c = document.getElementById("slate")
-//ctx = c.getContext("2d")
-//ctx.fillStyle = "#ffffff"
-//ctx.fillRect(0,0, c.height,c.width)
-//ctx.ellipse(10,10,10,10, 0, 0, Math.PI*2)
 
-function drawRec(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  ctx.fillStyle = "#000000"
-  ctx.fillRect(x-10,y-10,x+10,y+10)
+c = document.getElementById("slate");
+ctx = c.getContext("2d");
 
-}
+var clear = document.getElementById("clear");
 
-function drawDot(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  ctx.fillStyle = "#000000"
-  ctx.fillRect(x-10,y-10,x+10,y+10)
+clear.addEventListener( "click" , function() {
+    ctx.clearRect( 0, 0, c.width, c.height );
+});
 
-}
+c.addEventListener( "click" , function(event) {
+
+    if (rect) {
+        ctx.fillRect( event.offsetX, event.offsetY, 100, 100 );
+    } else {
+        ctx.beginPath();
+        ctx.ellipse( event.offsetX, event.offsetY, 20, 10, 0, 0, 2 * Math.PI );
+        ctx.fill();
+    }
+});
+
+var rect = false;
+var toggle = document.getElementById("toggle");
+
+toggle.addEventListener( "click" , function() {
+    rect = !rect;
+    if(rect){
+      toggle.innerHTML = "Current Mode: Rectangle";
+    }
+    else{
+      toggle.innerHTML = "Current Mode: Dot";
+    }
+
+});
