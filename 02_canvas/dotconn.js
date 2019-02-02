43b6ce93bb3703b7e0cb49bf99f3fde2c4b9a772
ivzhang1 <izhang1@stuy.edu>
2019-02-02 15:50:51 -0500

    done

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -1,28 +1,44 @@
 // team Northpoint (Ivan Zhang, Qian Zhou)
 // SoftDev2 pd7
-// K #01: ...and I want to Paint It Better
-// 2019-01-31
+// K #02: Connecting the Dots
+// 2019-02-02
 
 
 c = document.getElementById("playground");
 ctx = c.getContext("2d");
+isDots = false; // is true if there is at least one dot on canvas
+prevX = 0; // Store x cord of prev point
+prevY = 0; // Store y cord of prev point
 
 var clear = document.getElementById("clear");
 
 clear.addEventListener( "click" , function() {
 
-    ctx.clearRect( 0, 0, c.width, c.height );
-    //event.preventDefault();
-    ctx.beginPath();
+    ctx.clearRect( 0, 0, c.width, c.height ); //clears canvas
+    ctx.beginPath(); // resets the paths drawn, puts pen down
+    isDots = false; // resets isDots boolean
 
 });
 
 c.addEventListener( "click" , function(event) {
-  ctx.ellipse( event.offsetX, event.offsetY, 10, 10, 0, 0, 2 * Math.PI );
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.beginPath();
 
-    ctx.fill();
+    if(!isDots){
+      ctx.beginPath(); // puts pen down
+      ctx.ellipse( event.offsetX, event.offsetY, 10, 10, 0, 0, 2 * Math.PI );
+      ctx.fill(); //fills ellipse
+      isDots = true; 
+      x = event.offsetX;
+      y = event.offsetY;
+    }
+    else{
+      ctx.beginPath(); // puts pen down
+      ctx.moveTo(x,y); // moves pen to place of prev point
+      x = event.offsetX;
+      y = event.offsetY;
+      ctx.lineTo(x,y); // draws line to new point
+      ctx.ellipse( x, y, 10, 10, 0, 0, 2 * Math.PI ); // puts point down on new start of line
+      ctx.fill(); //fill in point
+    }
 
 
 });
