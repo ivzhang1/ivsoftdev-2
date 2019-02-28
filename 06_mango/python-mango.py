d193efdf8d303ec84564c412a8430baf0d943290
ivzhang1 <izhang1@stuy.edu>
2019-02-28 14:50:55 -0500

    add exmaple code

--- 06_mango/python-mango.py
+++ 06_mango/python-mango.py
@@ -1,4 +1,13 @@
 #Team Mangoose: Ivan Zhang and Sajed Nahian
 #SoftDev2 pd<7>
 #K #06: Yummy Mango Py
-#2019-02-28 
+#2019-02-29
+
+import pymongo
+
+server_add = "134.209.120.192"
+connection = pymongo.MongoClient(server_add)
+
+collection = ivan.restaurant
+
+print(collection)
