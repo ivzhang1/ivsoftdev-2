b7e8e180efd5b6bce45e9c9ea49b7550fec7d895
ivzhang1 <izhang1@stuy.edu>
2019-03-07 13:17:10 -0500

    add stuff

--- 08_mongosite/site.py
+++ 08_mongosite/site.py
@@ -1,5 +1,8 @@
-#Team ezræl - Ivan Zhang, William Lu, and Muhammed Uddin
+#Team ezræl - Ivan Zhang, William Lu, and Mohammed Uddin
 #SoftDev2 pd7
 #K #08: Ay Mon, Go Git It From Yer Flask
 #2019-03-07
 
+if __name__=="__main__":
+	app.debug=True
+	app.run()
