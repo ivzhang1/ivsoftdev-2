1bd19a02a1f6b3b3dfb71362f73b1ca1094220fc
ivzhang1 <izhang1@stuy.edu>
2019-03-12 22:26:24 -0400

    add JS to modify index.html

--- 09_svg/svg01.js
+++ 09_svg/svg01.js
@@ -9,20 +9,55 @@ var pic = document.getElementById("vimage");
 
 var clear = document.getElementById("but_clear");
 
+var prevX = null
+var prevY = null
+
 clear.addEventListener( "click" , function() {
-    ctx.clearRect( 0, 0, c.width, c.height );
+    while (pic.lastChild) {
+      pic.removeChild(pic.lastChild);
+    }
+    prevX = null
+    prevY = null
+    // var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    // rect.setAttribute("width", 500);
+    // rect.setAttribute("height", 500);
+    // rect.setAttribute("x", 0);
+    // rect.setAttribute("y", 0);
+    // rect.setAttribute("fill", "white");
+    //
+    // pic.appendChild(rect);
 });
 
 pic.addEventListener( "click" , function(event) {
-    //event.preventDefault();
-    console.log("HELLO")
-    var circle = document.createElementNS("https://www.w3.org/2000/svg", "circle")
-    circle.setAttribute("cx", event.offsetX)
-    circle.setAttribute("cy", event.offsetY)
-    circle.setAttribute("r", 10)
-    circle.setAttribute("stroke", "black")
-    circle.setAttribute("fill", "blue")
-    var pic = document.getElementById("vimage");
+    event.preventDefault();
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("cx", event.offsetX);
+    circle.setAttribute("cy", event.offsetY);
+    circle.setAttribute("r", 10);
+    circle.setAttribute("stroke", "black");
+    circle.setAttribute("fill", "blue");
+    //console.log(circle)
+    pic.appendChild(circle);
 
-    pic.appendChild(circle)
+    var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+    if(!prevX || !prevY){
+      prevX = event.offsetX;
+      prevY = event.offsetY;
+    }
+
+    line.setAttribute("x1", prevX);
+    line.setAttribute("x2", event.offsetX);
+    line.setAttribute("y1", prevY);
+    line.setAttribute("y2", event.offsetY);
+    line.setAttribute("stroke", "black");
+
+    prevX = event.offsetX;
+    prevY = event.offsetY;
+
+    pic.appendChild(line);
+
+
+
+
 });
