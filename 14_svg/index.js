89f55e5c12164f2a6cfb6601ed73826b8987dc5e
ivzhang1 <izhang1@stuy.edu>
2019-03-20 16:27:18 -0400

    test code from d3 HWS

--- 14_svg/index.js
+++ 14_svg/index.js
@@ -1,51 +1,32 @@
-var data = [4, 8, 15, 16, 23, 42];
-
-// var body = d3.select("body");
-// var div = body.append("div");
-// div.html("Hello, world!");
+var array = [42];
+var selection = d3.select("body");
+//var h2 = d3.selectAll("h2"); // Returns empty list if no h2
+//console.log(h2);
+var tds = d3.selectAll("tr").selectAll("td");
+//console.log(tds);
+var spans = d3.selectAll("tr").selectAll("td").selectAll("span");
+//console.log(spans);
+var sections = d3.selectAll("section");
+//console.log(sections);
+//console.log(sections.append("p")); // Returns a array of p
+//console.log(d3.selectAll("section").select("aside")); // Returns array of 4 items (aside for per 2 sections, 2 null)
+//document.body.__data__ = 42; // Nothing happens
+//d3.select("body").datum(42); // Nothing happens
+//d3.select("body").datum(42).append("h2"); // Adds data to h2, nothing displayed, require .join()
+var numbers = [4, 5, 18, 23, 42];
+var divs = d3.selectAll("h2").data(numbers)
+//divs.exit();
+divs.enter();
+var letters = [
+  {name: "A", frequency: .08167},
+  {name: "B", frequency: .01492},
+  {name: "C", frequency: .02780},
+  {name: "D", frequency: .04253},
+  {name: "E", frequency: .12702}
+];
 //
-// body.style("color", "white").style("background-color", "black");
-//
-// body.append("div").html("First!")
-// body.append("div").html("Second.");
-//
-// var x = d3.scaleLinear()
-//     .domain([0, d3.max(data)])
-//     .range([0, 420]);
-//
-// d3.select(".chart")
-//   .selectAll("div")
-//     .data(data)
-//   .enter().append("div")
-//     .style("width", function(d) { return x(d) + "px"; })
-//     .text(function(d) { return d; });
-var width = 420, barHeight = 20;
+// function names(freq) {
+//   return freq.name;
+// }
 
-var x = d3.scaleLinear()
-          .domain([0, d3.max(data)])
-          .range([0, width]);
-
-var chart = d3.select(".chart")
-              .attr("width", width)
-              .attr("height", barHeight * data.length);
-
-var bar = chart.selectAll("g")
-               .data(data)
-               .enter().append("g")
-               .attr("transform", function(d, i){
-                 return "translate(0," + i * barHeight + ")";
-               });
-
-bar.append("rect")
-   .attr("width", x)
-   .attr("height", barHeight - 1);
-
-bar.append("text")
-   .attr("x", function(d){
-     return x(d) - 3;
-   })
-   .attr("y", barHeight / 2)
-   .attr("dy", "0.35em")
-   .text(function(d){
-     return d;
-   });
+//console.log(d3.selectAll("div").data(letters, names));
