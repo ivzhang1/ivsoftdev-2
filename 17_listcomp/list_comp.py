6a9cff7ebc8d6e5392a678deb0a25fc44f101e42
ivzhang1 <izhang1@stuy.edu>
2019-04-24 19:28:48 -0400

    complete 1-4

---
+++ 17_listcomp/list_comp.py
@@ -0,0 +1,100 @@
+import math
+
+#1a
+l = []
+
+for i in range(5):
+	l.append(str(i*2)*2)
+
+print(l)
+
+#1b
+l = [str(i*2)*2 for i in range(5)]
+print(l)
+
+#2a
+l = []
+
+for i in range(5):
+	l.append(i*10+7)
+
+print(l)
+
+#2b
+l = [i*10+7 for i in range(5)]
+print(l)
+
+#3a
+l = []
+
+for i in range(3):
+	l.append(i*0)
+	l.append(i*1)
+	l.append(i*2)
+
+print(l)
+
+#3b
+l = [i*x for i in range(3) for x in range(3)]
+print(l)
+
+#4a Composite from [0,100] ascending
+l = []
+
+for i in range(1, 101):
+	for j in range(2, int(math.sqrt(i))+1):
+		if(i%j == 0 and i not in l):
+			l.append(i)
+
+print(l)
+
+#4b
+
+l = list({i for i in range(1, 101) for j in range(2, int(math.sqrt(i))+1) if i%j == 0})
+
+
+print(l)
+
+
+# #5a Prime from [0,100] ascending
+# l = []
+
+# for i in range(3):
+# 	l.append(i*0)
+# 	l.append(i*1)
+# 	l.append(i*2)
+
+# print(l)
+
+# #5b
+# l = [i*x for i in range(3) for x in range(3)]
+# print(l)
+
+
+# #6a Divisors in ascending
+# l = []
+
+# for i in range(3):
+# 	l.append(i*0)
+# 	l.append(i*1)
+# 	l.append(i*2)
+
+# print(l)
+
+# #6b
+# l = [i*x for i in range(3) for x in range(3)]
+# print(l)
+
+# #7a Transpose Matrix
+# l = []
+
+# for i in range(3):
+# 	l.append(i*0)
+# 	l.append(i*1)
+# 	l.append(i*2)
+
+# print(l)
+
+# #7b
+# l = [i*x for i in range(3) for x in range(3)]
+# print(l)
