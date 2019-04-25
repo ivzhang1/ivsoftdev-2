27abbc31c1ae034270619534d4db6488920df472
ivzhang1 <izhang1@stuy.edu>
2019-04-24 20:50:04 -0400

    made freq finders into functions

--- 20_anon-reduce/parser.py
+++ 20_anon-reduce/parser.py
@@ -3,12 +3,19 @@ text = f.read().strip().split(" ")
 f.close()
 
 word = raw_input("what word are you looking for: ").strip()
-correct_words = [x for x in text if x == word]
 
-print(len(correct_words))
+def find_word_freq(w):
+    correct_words = [x for x in text if x == w]
+
+    return(len(correct_words))
+
+print(find_word_freq(word))
 
 phrase = raw_input("what word phrase are you looking for: ").strip()
-phrase_combos = [text[i] + " " + text[i+1] + " " + text[i+2] for i in range(len(text)-2)]
-correct_phrases = [x for x in phrase_combos if x == phrase]
 
-print(len(correct_phrases))
+def find_word_phrase_freq(wp):
+    phrase_combos = [text[i] + " " + text[i+1] + " " + text[i+2] for i in range(len(text)-2)]
+    correct_phrases = [x for x in phrase_combos if x == wp]
+    return len(correct_phrases)
+
+print(find_word_phrase_freq(phrase))
