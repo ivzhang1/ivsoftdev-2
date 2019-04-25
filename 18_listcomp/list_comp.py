34c869b13799bd236b39578556f6e40828a66880
ivzhang1 <izhang1@stuy.edu>
2019-04-24 20:17:53 -0400

    finish quicksort

--- 18_listcomp/list_comp.py
+++ 18_listcomp/list_comp.py
@@ -5,3 +5,16 @@ n = int(raw_input("Enter an integer: "))
 pytha_triples = [[i, y, int(math.sqrt(i**2 + y**2))] for i in range(1, n+1) for y in range(1, n+1) if ((int(math.sqrt(i**2 + y**2))**2 == math.sqrt(i**2 + y**2)**2) and int(math.sqrt(i**2 + y**2)) <= n)]
 
 print(pytha_triples)
+
+def quicksort(list):
+    if (len(list) == 0):
+        return []
+    else:
+        pivot = list[0]
+        l = []
+        l = l + quicksort([x for x in list[1:] if x < pivot])
+        l.append(pivot)
+        l = l + quicksort([x for x in list[1:] if x >= pivot])
+        return l
+
+print(quicksort([2,3,5,1,1,-1,0,10,1]))
