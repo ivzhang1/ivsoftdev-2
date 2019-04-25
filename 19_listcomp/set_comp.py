80f4c91b41bd9124fe816c9d3f831e26477d7b77
ivzhang1 <izhang1@stuy.edu>
2019-04-24 20:27:48 -0400

    finish set_difference

--- 19_listcomp/set_comp.py
+++ 19_listcomp/set_comp.py
@@ -7,6 +7,12 @@ def intersection(s0, s1):
     l = union(s0, s1)
     l = filter(lambda x: x in s0, l)
     l = filter(lambda x: x in s1, l)
-    return l
+    return set(l)
 
 print(intersection({1,2,3}, {2,3,4}))
+
+def set_difference(s0, s1):
+    return set(filter(lambda x: x not in s1, s0))
+
+print(set_difference({1,2,3}, {2,3,4}))
+print(set_difference({2,3,4}, {1,2,3}))
