7dd8e07846e6f32c90ef39d3db4555cb8ca42dae
ivzhang1 <izhang1@stuy.edu>
2019-04-30 02:38:20 -0400

    finish parsing script

---
+++ 21_js-mfr/convert_to_json.py
@@ -0,0 +1,39 @@
+# Ivan Zhang
+# SoftDev2 pd7
+# K #21: Onions, Bell Peppers, and Celery, Oh My!
+# 2019-04-30
+
+f = open("schoolDemos.csv", "r")
+lines = f.readlines()
+f.close()
+categories = lines[0].split(",")
+lines = lines[1:]
+# print(lines)
+
+json = '{"results": ['
+limit = len(lines)
+count = 0
+
+for line in lines:
+    if count < limit:
+        json += "{"
+        if len(lines) < limit:
+            limit = len(lines)
+        for i in range(len(categories)):
+            l = line.split(",")
+            # print(categories[i], l[i])
+            json += '"{categ}" : "{val}",'.format(categ = categories[i].strip(), val = l[i].strip())
+        json = json[:-1]
+        json += "},"
+        count += 1
+    else:
+        break
+
+
+
+
+g = open("schoolDemos.json", "w")
+json = json[:-1]
+json += "]}"
+g.write(json)
+g.close()
---
+++ 21_js-mfr/data.js
@@ -0,0 +1,2 @@
+var obj = JSON.parse("schoolDemos.json").results
+console.log(obj)
